f24b4d4dddca0b1097c3c6b1d5c705ec0aed8736
inciner8r <punarvpawade17@gmail.com>
2022-02-04 10:41:32 +0530

    form and stuffs

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
+const article_router = require("./routes/articles");
 
 app.set("view engine", "ejs");
 
+app.use("/articles", article_router);
+
+const articles = [
+  {
+    title: "test",
+    createdAt: new Date(),
+    description: "test description",
+  },
+  {
+    title: "test 2",
+    createdAt: new Date(),
+    description: "test description 2",
+  },
+];
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.render("articles/index", { articles: articles });
 });
 
 app.listen(3000);
