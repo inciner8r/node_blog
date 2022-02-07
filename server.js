26d79f8e26b0885db42eeb9d1e61fe002a722c6a
inciner8r <punarvpawade17@gmail.com>
2022-02-07 11:12:04 +0530

    delet this nahui

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const Article = require("./models/article");
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/demo", {
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 app.use("/articles", article_router);
 
