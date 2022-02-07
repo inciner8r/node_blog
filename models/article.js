dc9d12416d2431938359187d369b23c8e3bf983e
inciner8r <punarvpawade17@gmail.com>
2022-02-07 10:16:45 +0530

    slug

--- models/article.js
+++ models/article.js
@@ -1,4 +1,6 @@
+const marked = require("marked");
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const articleSchema = new mongoose.Schema({
   title: {
@@ -16,6 +18,17 @@ const articleSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+articleSchema.pre("validate", async function () {
+  if (this.title) {
+    this.slug = await slugify(this.title, { lower: true, strict: true });
+  }
 });
 
 module.exports = mongoose.model("Article", articleSchema);
